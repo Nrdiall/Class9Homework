154b6cdbc83e8a912fb9a74aaaa36bae7bfbfa68
Naython <Naython@Naython.attlocal.net>
2017-09-27 00:03:59 -0500

    I created the service quality classes

---
+++ src/dip/lab2/FoodServiceTipCalculator.java
@@ -0,0 +1,25 @@
+package dip.lab2;
+
+/**
+ * An example low-level class. Does this class definition follow the DIP?
+ * If not, fix it.
+ *
+ * Any other best practice violations? Fix them too.
+ *
+ * @author your name goes here
+ */
+public class FoodServiceTipCalculator {
+    private static final double MIN_BILL = 0.00;
+    private static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
+    private static double GOOD_RATE = 0.20;
+    private static double FAIR_RATE = 0.15;
+    private static double POOR_RATE = 0.10;
+
+    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
+        
+    }
+
+   
+
+}
---
+++ src/dip/lab2/ServiceQualityRates.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+/**
+ *
+ * @author Naython
+ */
+public class ServiceQualityRates {
+    private static double GOOD_RATE = 0.20;
+    private static double FAIR_RATE = 0.15;
+    private static double POOR_RATE = 0.10;
+    
+    
+}
