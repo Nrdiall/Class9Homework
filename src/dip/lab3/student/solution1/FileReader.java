1d7a9f00472e027b287511da6f6b9c272a4031d7
Naython <Naython@Naython.attlocal.net>
2017-09-27 16:40:15 -0500

    Created all classes for lab 3

---
+++ src/dip/lab3/student/solution1/FileReader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3.student.solution1;
+
+/**
+ *
+ * @author Naython
+ */
+public class FileReader implements Reader{
+
+    @Override
+    public String readMessage() {
+        
+    }
+    
+}
---
+++ src/dip/lab3/student/solution1/FileWriter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3.student.solution1;
+
+/**
+ *
+ * @author Naython
+ */
+public class FileWriter implements Writer{
+
+    @Override
+    public void writeMessage(String m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
