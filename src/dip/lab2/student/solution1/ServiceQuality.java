6f8d5ab5cff2da80cd014aff834127a741000615
Naython <Naython@Naython.attlocal.net>
2017-09-27 01:30:47 -0500

    Realized that I didn't put the classes in the right package. oops.

--- src/dip/lab2/student/solution1/ServiceQuality.java
+++ src/dip/lab2/student/solution1/ServiceQuality.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dip.lab2;
+package dip.lab2.student.solution1;
 
 /**
  *
  * @author Naython
  */
-public interface TipCalculator {
-    public abstract double getTip();
+public enum ServiceQuality {
+    POOR, FAIR, GOOD;
 }
--- src/dip/lab2/student/solution1/TipCalculator.java
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dip.lab2;
+package dip.lab2.student.solution1;
 
 /**
  *
  * @author Naython
  */
-public enum ServiceQuality {
-    POOR, FAIR, GOOD;
+public interface TipCalculator {
+    public abstract double getTip();
 }
